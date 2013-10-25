1acc4c76471308304aeba21360484fd63f8a432f
Martin <martol01@abv.bg>
2013-10-25 13:48:24 +0100

    updating company

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <html>
 <head>
 <Title>Registration Form</Title>
@@ -21,6 +22,7 @@
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
+ Company Name <input type="text" name="company_name" id="company_name"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
@@ -44,14 +46,16 @@
     try {
         $name = $_POST['name'];
         $email = $_POST['email'];
+$company_name=$_POST['company_name'];
         $date = date("Y-m-d");
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
+        $sql_insert = "INSERT INTO registration_tbl (name, email, company_name,date) 
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $date);
+        $stmt->bindValue(3, $company_name);
+$stmt->bindValue(4, $date);
         $stmt->execute();
     }
     catch(Exception $e) {
@@ -68,10 +72,12 @@
         echo "<table>";
         echo "<tr><th>Name</th>";
         echo "<th>Email</th>";
+echo "<th>Company Name</th>";
         echo "<th>Date</th></tr>";
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
+echo "<td>".$registrant['company_name']."</td>";
             echo "<td>".$registrant['date']."</td></tr>";
         }
         echo "</table>";
@@ -81,3 +87,4 @@
 ?>
 </body>
 </html>
+
